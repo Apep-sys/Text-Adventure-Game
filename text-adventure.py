40f1e0a7354cb3e78dc7a1765e34d11e10b88864
Dragos <mailsafe923@gmail.com>
2023-07-02 01:34:00 +0300

    Added intro and details for the fantasy world and reworked its elements. The third choice doesn't match the list index(3 over 2) so that must be changed on in the f string.

--- text-adventure.py
+++ text-adventure.py
@@ -4,22 +4,34 @@ class Player:
         return self.choice
 
     def creation(self, place_choice):
-        fantasy_options = ['adventurer', 'order']  # De facut clase pt fiecare optiune
+        fantasy_options = ['Knight', 'Mage', 'Druid']  # De facut clase pt fiecare optiune
         forest_options = ['Wawanakwa', 'Red Root', 'Blue Dawn']
-        murder_mystery = ['endorsed guest', 'room']
+        murder_mystery = ['09', '13', '256']
         if place_choice == 1:
             self.name = input("What is your name, scout?\n")
-            self.pclass = int(input("And what camp do you come from?\n"))
+            self.pclass = int(input("And what camp do you come from?\n"
+                                    "1. Camp Wawanakwa\n"
+                                    "2. Camp Red Root\n"
+                                    "3. Camp Blue Dawn\n"))
             self.place = 'Taibur Forest'
             self.detail1 = ['mapping', 'exploring', 'searching for tracks in the']
             self.intro = f"Welcome, {self.name}, to {self.place}. You were tasked by Camp {forest_options[self.pclass]}" \
-                         f"with {self.detail1[self.pclass]} the {self.place}. Carefully weigh your choices, as your" \
-                         f"survival depends on them. The night sets in now...and you must survive it.  "
+                         f"with {self.detail1[self.pclass]} the Taibur Forest.\nCarefully weigh your choices, as your" \
+                         f"survival depends on them. \nThe night sets in now...and you must survive it.  "
 
         if place_choice == 2:
             self.name = input("What is your name, adventurer?\n")
-            self.pclass = int(input("And what order do you belong to?\n"))
-            self.place = 'Country of Maglana'
+            self.pclass = int(input("And what order do you belong to?\n"
+                                    "1. Knight\n"
+                                    "2. Mage\n"
+                                    "3. Druid\n"))
+            self.place = 'Cave of Magtarr'
+            self.detail1 = ['looking for the lost group', 'restoring the natural balance', 'defeating the creature']
+            self.intro = f"I welcome thee, {fantasy_options[self.pclass]} {self.name} of the Great Country of Delirion.\n" \
+                         f"Carefully listen, as you have been tasked with {self.detail1[self.pclass]} of the Cave of" \
+                         f"Magtarr. \nNow, heed my call, as the challenges you will face must be met with proper thought" \
+                         f"and strength of mind and body. \nMake it to the end of the cave and finish your task...but" \
+                         f"beware the horrors in the dark."
         if place_choice == 3:
             self.name = input("What is your name, endorsed guest?\n")
             self.pclass = int(input("And what room are you from?\n"))
@@ -62,6 +74,7 @@ class Game(Player):
               f"3. {Game.places[2]}\n")
         player_choice = player.make_choice()
         player.creation(player_choice)
+        print(player.intro)
 
 player = Player()
 game = Game()
