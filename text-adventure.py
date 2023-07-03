352792c327ffd7a295cd60bcaf60432237e97132
Apep <mailsafe923@gmail.com>
2023-07-03 21:40:53 +0300

    Added intro and detail1 for the Murder Mystery world. Added narration selection for the different phases. Added beginning of the function for the First Room -> working on Murder Mystery.

--- text-adventure.py
+++ text-adventure.py
@@ -6,7 +6,7 @@ class Player:
     def creation(self, place_choice):
         fantasy_options = ['Knight', 'Mage', 'Druid']  # De facut clase pt fiecare optiune
         forest_options = ['Wawanakwa', 'Red Root', 'Blue Dawn']
-        murder_mystery = ['09', '13', '256']
+        murder_options = ['09', '13', '256']
         if place_choice == 1:
             self.name = input("What is your name, scout?\n")
             self.pclass = int(input("And what camp do you come from?\n"
@@ -15,8 +15,8 @@ class Player:
                                     "3. Camp Blue Dawn\n"))
             self.place = 'Taibur Forest'
             self.detail1 = ['mapping', 'exploring', 'searching for tracks in the']
-            self.intro = f"Welcome, {self.name}, to {self.place}. You were tasked by Camp {forest_options[self.pclass]}" \
-                         f"with {self.detail1[self.pclass]} the Taibur Forest.\nCarefully weigh your choices, as your" \
+            self.intro = f"Welcome, {self.name}, to {self.place}. You were tasked by Camp {forest_options[self.pclass - 1]}" \
+                         f"with {self.detail1[self.pclass - 1]} the Taibur Forest.\nCarefully weigh your choices, as your" \
                          f"survival depends on them. \nThe night sets in now...and you must survive it.  "
 
         if place_choice == 2:
@@ -27,22 +27,30 @@ class Player:
                                     "3. Druid\n"))
             self.place = 'Cave of Magtarr'
             self.detail1 = ['looking for the lost group', 'restoring the natural balance', 'defeating the creature']
-            self.intro = f"I welcome thee, {fantasy_options[self.pclass]} {self.name} of the Great Country of Delirion.\n" \
-                         f"Carefully listen, as you have been tasked with {self.detail1[self.pclass]} of the Cave of" \
-                         f"Magtarr. \nNow, heed my call, as the challenges you will face must be met with proper thought" \
+            self.intro = f"I welcome thee, {fantasy_options[self.pclass - 1]} {self.name} of the Great Country of Delirion.\n" \
+                         f"Carefully listen, as you have been tasked with {self.detail1[self.pclass - 1]} of the {self.place}." \
+                         f"\nNow, heed my call, as the challenges you will face must be met with proper thought" \
                          f"and strength of mind and body. \nMake it to the end of the cave and finish your task...but" \
                          f"beware the horrors in the dark."
         if place_choice == 3:
             self.name = input("What is your name, endorsed guest?\n")
             self.pclass = int(input("And what room are you from?\n"))
             self.place = 'Hotel Margot'
+            self.detail1 = ['fashion gala, in the main hall', 'interview for your paper, in the main hall',
+                            'guests\'s appetite, in the main hall,']
+            self.intro = f"Oh, dear! If it isn't {self.name}! We were expecting you, and we are pleased to have you.\n" \
+                         f"Please, make yourself at home in our dear {self.place}. Your room is {murder_options[self.pclass - 1]}.\n" \
+                         f"For tonight, the {self.detail1[self.pclass - 1]} awaits you. Do consider taking your time when" \
+                         f"deciding how to best approach this event!\nIt won't be long until the MAIN event will begin..." \
+                         f"Consider yourself lucky for the heads up. The other guests aren't so lucky. \nOne, in particular..\n" \
+                         f"But enough talk! Go on! Have a wonderful evening!\nMaybe the last one. "
         self.state = 'alive'
 
     def change_state(self):
         self.state = 'dead'
 
 class Game(Player):
-    phases = 5
+    phases = 1
     places = [' forest trip', 'fantasy adventure', 'murder mystery']
     adventure_count = 0
 
@@ -65,9 +73,18 @@ class Game(Player):
         print("... ... ... ... YOU ARE SUCH A DISAPPOINTMENT. YOU. ARE. DEAD.\n")
 
     def game_start(self, player):
-        # De adaugat anunt cu X din Y a venit etc.
         Game.adventure_count += 1
-        Game.narrator1(self)
+        if Game.phases == 1:
+            Game.narrator1(self)
+        elif Game.phases == 2:
+            Game.narrator2(self)
+        elif Game.phases == 3:
+            Game.narrator3(self)
+        elif Game.phases == 4:
+            Game.narrator4(self)
+        elif Game.phases == 5:
+            Game.narrator5(self)
+
         print(f"Your choice for your setting is...: \n"
               f"1.{Game.places[0]}\n"
               f"2. {Game.places[1]}\n"
@@ -75,6 +92,10 @@ class Game(Player):
         player_choice = player.make_choice()
         player.creation(player_choice)
         print(player.intro)
+
+    def first_room(self, player):
+        # First room to be added! WIP.
+
 
 player = Player()
 game = Game()
