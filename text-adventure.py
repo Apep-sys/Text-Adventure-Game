d5d419e23c53ec0c44a1af8038bf9e692b80f10d
Apep <mailsafe923@gmail.com>
2023-07-07 11:20:25 +0300

    Test again

--- text-adventure.py
+++ text-adventure.py
@@ -132,7 +132,7 @@ class Player:
                               'Cause of death: Asphyxiation.\n')
                         self.change_state()
                     if self.action1.lower() == 'balcony':
-                        print('The wind lifts your hair...')aw
+                        print('The wind lifts your hair...')awa
                     # Need to finish the rest of the rooms.
 
 
